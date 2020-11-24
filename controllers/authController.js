eafae1a06a40c2b2cb1dfc05519889c1c24e41a9
François Behague <behaque@nycticorax.org>
2020-11-24 16:43:25 +0100

    Finished Node auth tutorial episode 10,11,12,13,14,15,16,17,18

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,21 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken')
 
 // handle errors
 const handleErrors = (err) => {
 console.log(err.message, err.code);
 let errors = { email: '', password: ''};
 
+// incorrect email
+if (err.message === 'incorrect email') {
+  errors.email = 'that email is not registered'
+  
+}
+// incorrect password
+if (err.message === 'incorrect password') {
+  errors.password = 'that password is incorrect'
+  
+}
 // duplicate error code
 if (err.code === 11000) {
   errors.email = 'that email is already registered';
@@ -20,6 +31,14 @@ if (err.message.includes('user validation failed')){
 
   return errors;
 }
+
+// create json web token
+const createToken = (id) => {
+  return jwt.sign({ id }, 'frivoc secret', {
+    expiresIn: "31d"
+  })
+}
+
 module.exports.signup_get = (req,res) => {
   res.render('signup');
 }
@@ -32,18 +51,34 @@ module.exports.signup_post = async (req,res) => {
   const { email, password } = req.body;
 
   try {
-  const user = await User.create({ email, password });
-  res.status(201).json(user);
+    const user = await User.create({ email, password });
+    const token = createToken(user._id)
+    res.cookie('jwt', token, { httpOnly: true, maxAge: 60 * 60 * 24 * 31 });
+    res.status(201).json({ user: user._id });
   }
   catch(err){
-  const errors = handleErrors(err);
-  res.status(400).json({ errors });
+    const errors = handleErrors(err);
+    res.status(400).json({ errors });
   }
 }
 
 module.exports.login_post = async (req,res) => {
   const { email, password } = req.body;
 
-  console.log(email, password);
-  res.send('user login');
+  try {
+    const user = await User.login(email, password)
+    const token = createToken(user._id)
+    res.cookie('jwt', token, { httpOnly: true, maxAge: 60 * 60 * 24 * 31 });
+    res.status(200).json({ user: user.id})
+    
+  }
+  catch (err) {
+    const errors = handleErrors(err);
+    res.status(400).json({ errors })
+  }
+}
+
+module.exports.logout_get = (req,res) => {
+  res.cookie('jwt', '', { maxAge: 1 });
+  res.redirect('/');
 }
